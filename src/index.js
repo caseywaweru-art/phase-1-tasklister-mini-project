d3dbf9064c58b28ab805bd1f2849e8c19adc0393
Casey Waweru <casey.waweru@gmail.com>
2025-09-25 15:18:06 +0300

    Functionality to append data to the todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const formSubmit = document.getElementById("create-task-form");
+
+/*formSubmit.addEventListener("click", function(event){
+  event.PreventDefault();
+  console.log("Hello there");
+});*/
+
+formSubmit.addEventListener("submit", (event) => {
+  //console.log("Hello there");
+  addingTasks();
+
+  event.preventDefault();
 });
+
+// Function to handle adding new tasks to the list
+function addingTasks() {
+  const list = document.getElementById("list");
+  const tasks = document.getElementById("tasks");
+  const newTask = document.getElementById("new-task-description");
+  // Creates a new list element
+  const li = document.createElement("li");
+
+  // Adds the input from the form to the list element
+  li.textContent = newTask.value;
+  // Appends the list element to the ul
+  tasks.append(li);
+}
